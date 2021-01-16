c5df277dc5cad245ee495040003674119ed1b163
kh <kitt.hsu@gmail.com>
2021-01-16 22:45:48 +0800

    multiple sessions share the same debugger pod

--- pkg/cmd/debug.go
+++ pkg/cmd/debug.go
@@ -17,6 +17,7 @@ package cmd
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/warm-metal/kubectl-dev/pkg/kubectl"
@@ -25,12 +26,15 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/rand"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/scale/scheme/extensionsv1beta1"
 	"os"
 	"strings"
@@ -48,6 +52,9 @@ type DebugOptions struct {
 	container        string
 	installCSIDriver bool
 	minikube         bool
+	createNew        bool
+	debuggerPodName  string
+	keepDebugger bool
 
 	id          string
 	instance    string
@@ -69,33 +76,34 @@ func NewDebugOptions(streams genericclioptions.IOStreams) *DebugOptions {
 }
 
 func (o *DebugOptions) Complete(cmd *cobra.Command, args []string) error {
-	if len(args) < 1 || len(args) > 2 {
-		return xerrors.Errorf("must specify a kind and an object, or an image. See the usage")
-	}
-
 	if o.Raw().Namespace != nil && len(*o.Raw().Namespace) > 0 {
 		o.namespace = *o.Raw().Namespace
 	}
 
-	// FIXME args[0] can be "po/name" other than an image
-	if len(args) == 1 {
-		o.image = args[0]
-		o.instance = fmt.Sprintf("debugger-image-%s", o.id)
-		o.labels = map[string]string{
-			"debugger.warm-metal.tech/image": "",
+	if len(args) == 0 {
+		if len(o.image) == 0 {
+			return xerrors.Errorf("specify an image or an object")
 		}
+
+		encodedImage := base64.StdEncoding.EncodeToString([]byte(o.image))
+		o.instance = fmt.Sprintf("debugger-%s-%s", encodedImage, o.id)
+		o.labels = map[string]string{
+			"debugger.warm-metal.tech/image": encodedImage,
+		}
+
 		return nil
 	}
 
-	o.instance = fmt.Sprintf("debugger-%s-%s-%s", args[0], args[1], o.id)
-	o.labels = map[string]string{
-		"debugger.warm-metal.tech/kind": args[0],
-		"debugger.warm-metal.tech/name": args[1],
-	}
-
-	podTmpl, err := o.fetchPod(o.namespace, args)
+	kind, name, podTmpl, err := o.fetchPod(o.namespace, args)
 	if err != nil {
 		return err
+	}
+
+	o.instance = fmt.Sprintf("debugger-%s-%s-%s", kind, name, o.id)
+	o.labels = map[string]string{
+		"debugger.warm-metal.tech/kind":      kind,
+		"debugger.warm-metal.tech/name":      name,
+		"debugger.warm-metal.tech/container": o.container,
 	}
 
 	if len(podTmpl.Containers) == 1 && len(o.container) == 0 {
@@ -109,7 +117,8 @@ func (o *DebugOptions) Complete(cmd *cobra.Command, args []string) error {
 			containers = append(containers, c.Name)
 		}
 
-		return xerrors.Errorf("%#v has more than 1 container. Specify one of %#v via -c", args, containers)
+		return xerrors.Errorf("%s/%s has more than 1 container. Specify one of %#v via -c",
+			kind, name, containers)
 	}
 
 	for i, c := range o.podTmpl.Containers {
@@ -121,7 +130,7 @@ func (o *DebugOptions) Complete(cmd *cobra.Command, args []string) error {
 	}
 
 	if o.podTmpl == nil {
-		return xerrors.Errorf("container %s doesn't found in %#v", o.container, args)
+		return xerrors.Errorf("container %s doesn't found in %s/%s", o.container, kind, name)
 	}
 
 	return nil
@@ -147,7 +156,9 @@ func (o *DebugOptions) loadDefaultTemplate() {
 	}
 }
 
-func (o *DebugOptions) fetchPod(namespace string, kindAndName []string) (*corev1.PodSpec, error) {
+func (o *DebugOptions) fetchPod(
+	namespace string, kindAndName []string,
+) (kind, name string, podTmpl *corev1.PodSpec, err error) {
 	result := resource.NewBuilder(o.Raw()).
 		Unstructured().
 		ContinueOnError().
@@ -157,16 +168,19 @@ func (o *DebugOptions) fetchPod(namespace string, kindAndName []string) (*corev1
 		Flatten().
 		Do()
 	if result.Err() != nil {
-		return nil, xerrors.Errorf(`can't fetch "%#v": %s`, kindAndName, result.Err())
+		err = xerrors.Errorf(`can't fetch "%#v": %s`, kindAndName, result.Err())
+		return
 	}
 
 	infos, err := result.Infos()
 	if err != nil {
-		return nil, xerrors.Errorf(`can't fetch result of "%#v": %s`, kindAndName, result.Err())
+		err = xerrors.Errorf(`can't fetch result of "%#v": %s`, kindAndName, result.Err())
+		return
 	}
 
 	if len(infos) == 0 {
-		return nil, xerrors.Errorf(`no "%#v" found`, kindAndName)
+		err = xerrors.Errorf(`no "%#v" found`, kindAndName)
+		return
 	}
 
 	if len(infos) > 1 {
@@ -175,29 +189,32 @@ func (o *DebugOptions) fetchPod(namespace string, kindAndName []string) (*corev1
 
 	clientset, err := o.ClientSet()
 	if err != nil {
-		return nil, err
+		return
 	}
 
-	var podTmpl *corev1.PodSpec
 	ctx := context.TODO()
 	opt := metav1.GetOptions{}
 	info := infos[0]
+	kind = strings.ToLower(info.Mapping.GroupVersionKind.Kind)
+	name = info.Name
 	switch info.Mapping.GroupVersionKind.Kind {
 	case "Deployment":
 		switch info.Mapping.GroupVersionKind.Group {
 		case extensionsv1beta1.GroupName:
-			deploy, err := clientset.ExtensionsV1beta1().
+			deploy, failed := clientset.ExtensionsV1beta1().
 				Deployments(info.Namespace).
 				Get(ctx, info.Name, opt)
-			if err != nil {
-				return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+			if failed != nil {
+				err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+				return
 			}
 
 			podTmpl = &deploy.Spec.Template.Spec
 		case appsv1.GroupName:
-			deploy, err := clientset.AppsV1().Deployments(info.Namespace).Get(ctx, info.Name, opt)
-			if err != nil {
-				return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+			deploy, failed := clientset.AppsV1().Deployments(info.Namespace).Get(ctx, info.Name, opt)
+			if failed != nil {
+				err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+				return
 			}
 
 			podTmpl = &deploy.Spec.Template.Spec
@@ -209,20 +226,22 @@ func (o *DebugOptions) fetchPod(namespace string, kindAndName []string) (*corev1
 			panic(infos[0].Mapping.GroupVersionKind)
 		}
 
-		sfst, err := clientset.AppsV1().StatefulSets(info.Namespace).Get(ctx, info.Name, opt)
-		if err != nil {
-			return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+		sfs, failed := clientset.AppsV1().StatefulSets(info.Namespace).Get(ctx, info.Name, opt)
+		if failed != nil {
+			err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+			return
 		}
 
-		podTmpl = &sfst.Spec.Template.Spec
+		podTmpl = &sfs.Spec.Template.Spec
 	case "Job":
 		if info.Mapping.GroupVersionKind.Group != batchv1.GroupName {
 			panic(infos[0].Mapping.GroupVersionKind)
 		}
 
-		job, err := clientset.BatchV1().Jobs(info.Namespace).Get(ctx, info.Name, opt)
-		if err != nil {
-			return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+		job, failed := clientset.BatchV1().Jobs(info.Namespace).Get(ctx, info.Name, opt)
+		if failed != nil {
+			err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+			return
 		}
 
 		podTmpl = &job.Spec.Template.Spec
@@ -231,25 +250,28 @@ func (o *DebugOptions) fetchPod(namespace string, kindAndName []string) (*corev1
 			panic(info.Mapping.GroupVersionKind)
 		}
 
-		job, err := clientset.BatchV1beta1().CronJobs(info.Namespace).Get(ctx, info.Name, opt)
-		if err != nil {
-			return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+		job, failed := clientset.BatchV1beta1().CronJobs(info.Namespace).Get(ctx, info.Name, opt)
+		if failed != nil {
+			err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+			return
 		}
 
 		podTmpl = &job.Spec.JobTemplate.Spec.Template.Spec
 	case "DaemonSet":
 		switch info.Mapping.GroupVersionKind.Group {
 		case extensionsv1beta1.GroupName:
-			ds, err := clientset.ExtensionsV1beta1().DaemonSets(info.Namespace).Get(ctx, info.Name, opt)
-			if err != nil {
-				return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+			ds, failed := clientset.ExtensionsV1beta1().DaemonSets(info.Namespace).Get(ctx, info.Name, opt)
+			if failed != nil {
+				err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+				return
 			}
 
 			podTmpl = &ds.Spec.Template.Spec
 		case appsv1.GroupName:
-			ds, err := clientset.AppsV1().DaemonSets(info.Namespace).Get(ctx, info.Name, opt)
-			if err != nil {
-				return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+			ds, failed := clientset.AppsV1().DaemonSets(info.Namespace).Get(ctx, info.Name, opt)
+			if failed != nil {
+				err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+				return
 			}
 
 			podTmpl = &ds.Spec.Template.Spec
@@ -259,16 +281,18 @@ func (o *DebugOptions) fetchPod(namespace string, kindAndName []string) (*corev1
 	case "ReplicaSet":
 		switch info.Mapping.GroupVersionKind.Group {
 		case extensionsv1beta1.GroupName:
-			rs, err := clientset.ExtensionsV1beta1().ReplicaSets(info.Namespace).Get(ctx, info.Name, opt)
-			if err != nil {
-				return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+			rs, failed := clientset.ExtensionsV1beta1().ReplicaSets(info.Namespace).Get(ctx, info.Name, opt)
+			if failed != nil {
+				err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+				return
 			}
 
 			podTmpl = &rs.Spec.Template.Spec
 		case appsv1.GroupName:
-			rs, err := clientset.AppsV1().ReplicaSets(info.Namespace).Get(ctx, info.Name, opt)
-			if err != nil {
-				return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+			rs, failed := clientset.AppsV1().ReplicaSets(info.Namespace).Get(ctx, info.Name, opt)
+			if failed != nil {
+				err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+				return
 			}
 
 			podTmpl = &rs.Spec.Template.Spec
@@ -280,18 +304,79 @@ func (o *DebugOptions) fetchPod(namespace string, kindAndName []string) (*corev1
 			panic(info.Mapping.GroupVersionKind)
 		}
 
-		pod, err := clientset.CoreV1().Pods(info.Namespace).Get(ctx, info.Name, opt)
-		if err != nil {
-			return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+		pod, failed := clientset.CoreV1().Pods(info.Namespace).Get(ctx, info.Name, opt)
+		if failed != nil {
+			err = xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, failed)
+			return
 		}
 
 		podTmpl = &pod.Spec
 	default:
-		err = fmt.Errorf("%s not found. Forgot a namespace? ", strings.Join(kindAndName, " "))
-		return nil, xerrors.Errorf("can't fetch %s/%s: %s", info.Mapping.GroupVersionKind, info.Name, err)
+		err = xerrors.Errorf("object %s/%s is not supported", info.Mapping.GroupVersionKind, info.Name)
+		return
 	}
 
-	return podTmpl, nil
+	return
+}
+
+func (o *DebugOptions) fetchDebugger(clientSet *kubernetes.Clientset) (debugger, container string, err error) {
+	opts := metav1.ListOptions{
+		LabelSelector: labels.SelectorFromSet(o.labels).String(),
+	}
+
+	if len(o.debuggerPodName) > 0 {
+		opts.FieldSelector = fields.Set{"metadata.name": o.debuggerPodName}.AsSelector().String()
+	}
+
+	debuggerList, err := clientSet.CoreV1().Pods(o.namespace).List(context.TODO(), opts)
+	if err != nil {
+		if !errors.IsNotFound(err) {
+			err = nil
+			return
+		}
+
+		err = xerrors.Errorf("can't fetch debugger: %s", err)
+		return
+	}
+
+	if len(debuggerList.Items) == 0 {
+		return
+	}
+
+	if len(debuggerList.Items) > 1 {
+		err = xerrors.Errorf("found multiple debugger Pods. Try --debugger.")
+		return
+	}
+
+	debugger = debuggerList.Items[0].Name
+	if len(debuggerList.Items[0].Spec.Containers) == 1 {
+		container = debuggerList.Items[0].Spec.Containers[0].Name
+		if len(o.container) > 0 && container != o.container {
+			err = xerrors.Errorf("container %s doesn't found in debugger %s. Try %s or remove -c",
+				o.container, debugger)
+		}
+
+		return
+	}
+
+	if len(o.container) == 0 {
+		var containers []string
+		for _, c := range debuggerList.Items[0].Spec.Containers {
+			containers = append(containers, c.Name)
+		}
+
+		err = xerrors.Errorf("debugger %s has more than 1 container. Specify one of %#v via -c",
+			debuggerList, containers)
+		return
+	}
+
+	for _, c := range debuggerList.Items[0].Spec.Containers {
+		if c.Name == o.container {
+			container = c.Name
+		}
+	}
+
+	return
 }
 
 const (
@@ -316,6 +401,23 @@ func (o *DebugOptions) Run() error {
 		fmt.Println("install manifests")
 		if err := kubectl.ApplyManifests(manifest); err != nil {
 			return err
+		}
+	}
+
+	clientset, err := o.ClientSet()
+	if err != nil {
+		return err
+	}
+
+	if !o.createNew {
+		debugger, container, err := o.fetchDebugger(clientset)
+		if err != nil {
+			return err
+		}
+
+		if len(debugger) > 0 {
+			fmt.Printf("Start bash of debugger Pod %s\n", debugger)
+			return kubectl.Exec(debugger, o.namespace, container, "bash")
 		}
 	}
 
@@ -362,7 +464,7 @@ func (o *DebugOptions) Run() error {
 		MountPath: "/image",
 	})
 
-	pod := &corev1.Pod{
+	debugger := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:   o.instance,
 			Labels: o.labels,
@@ -370,20 +472,15 @@ func (o *DebugOptions) Run() error {
 		Spec: *o.podTmpl,
 	}
 
-	clientset, err := o.ClientSet()
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Create debugger Pod %s...\n", pod.Name)
-	newPod, err := clientset.CoreV1().Pods(o.namespace).Create(context.TODO(), pod, metav1.CreateOptions{})
+	fmt.Printf("Create debugger Pod %s...\n", debugger.Name)
+	newDebugger, err := clientset.CoreV1().Pods(o.namespace).Create(context.TODO(), debugger, metav1.CreateOptions{})
 	if err != nil {
 		return err
 	}
 
 	fmt.Println("waiting Pod ready")
 	watcher, err := clientset.CoreV1().Pods(o.namespace).Watch(context.TODO(), metav1.ListOptions{
-		FieldSelector: fields.Set{"metadata.name": newPod.Name}.AsSelector().String(),
+		FieldSelector: fields.Set{"metadata.name": newDebugger.Name}.AsSelector().String(),
 		Watch:         true,
 	})
 
@@ -392,25 +489,28 @@ func (o *DebugOptions) Run() error {
 	}
 
 	err = utils.WaitUntilErrorOr(watcher, func(object runtime.Object) (b bool, err error) {
-		fmt.Printf("Debugger %s updated\n", newPod.Name)
+		fmt.Printf("Debugger %s updated\n", newDebugger.Name)
 		return object.(*corev1.Pod).Status.Phase == corev1.PodRunning, nil
 	})
 
 	if err != nil {
-		return xerrors.Errorf("can't start debugger Pod %s: %s", newPod.Name, err)
+		return xerrors.Errorf("can't start debugger Pod %s: %s", newDebugger.Name, err)
 	}
 
-	fmt.Printf("Debugger Pod %s is ready\n", newPod.Name)
+	fmt.Printf("Debugger Pod %s is ready\n", newDebugger.Name)
 
-	defer func() {
-		err = clientset.CoreV1().Pods(o.namespace).Delete(context.TODO(), newPod.Name, metav1.DeleteOptions{})
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "can't delete the pod debugger: %s\n", err)
-		}
-	}()
+	if !o.keepDebugger {
+		fmt.Println("will be destroyed after session closed.")
+		defer func() {
+			fmt.Printf("Destroy debugger %s/%s...\n", newDebugger.Namespace, newDebugger.Name)
+			if err = kubectl.Delete("Pod", newDebugger.Name, newDebugger.Namespace); err != nil {
+				fmt.Fprintf(os.Stderr, "can't delete the debugger debugger: %s\n", err)
+			}
+		}()
+	}
 
-	fmt.Printf("Start bash of debugger Pod %s\n", newPod.Name)
-	return kubectl.Exec(newPod.Name, newPod.Namespace, container.Name, "bash")
+	fmt.Printf("Start bash of debugger Pod %s\n", newDebugger.Name)
+	return kubectl.Exec(newDebugger.Name, newDebugger.Namespace, container.Name, "bash")
 }
 
 func NewCmdDebug(streams genericclioptions.IOStreams) *cobra.Command {
@@ -457,7 +557,7 @@ kubectl dev debug image-name
 	}
 
 	cmd.Flags().BoolVar(&o.minikube, "minikube", o.minikube,
-		"If true, the target cluster is assumed to be a minikube cluster.")
+		"If set, the target cluster is assumed to be a minikube cluster.")
 	cmd.Flags().StringVar(
 		&o.debugBaseImage, "base", o.debugBaseImage,
 		`Base image used to mount the target image. "bash" is required in the base image`)
@@ -466,7 +566,14 @@ kubectl dev debug image-name
 		"Container of the specified object if in which there are multiple containers")
 	cmd.Flags().BoolVar(&o.installCSIDriver, "also-apply-csi-driver", false,
 		`Apply the CSI driver "https://github.com/warm-metal/csi-driver-image". The driver is required. If you already have it installed, turn it off.`)
-
+	cmd.Flags().BoolVar(&o.createNew, "create-new", false,
+		"If set, always creates a new debugger Pod")
+	cmd.Flags().StringVar(&o.image, "image", "",
+		"The target image. If not set, use the image which the object used.")
+	cmd.Flags().StringVar(&o.debuggerPodName, "debugger", "",
+		"Debugger Pod name. If set along with --create-new=false, will find debugger has the specified name.")
+	cmd.Flags().BoolVar(&o.keepDebugger, "keep-debugger", false,
+		"If set, the debugger Pod won't be destroyed after the session closed.")
 	o.AddFlags(cmd.Flags())
 
 	return cmd
